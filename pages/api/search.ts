0ba3aad198731df68f5f5f5123f0a4815744fae6
Surya <git@sdan.io>
2023-03-06 21:21:30 -0800

    fix bugs and remove logs

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,26 +1,21 @@
 // write a search function that returns a list of posts by pinging the OpenAI API
 // and then return the results as JSON
 
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next";
 const { Configuration, OpenAIApi } = require("openai");
 
-
-
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-  
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const search = async (req: NextApiRequest, res: NextApiResponse) => {
-    // fetch query from request body
-    const { q } = req.body
-
-
-  console.log("api in search function", q)
-  const prompting = "You are a search engine that provides useful information to a given search query. Given a query you are to act like a search engine and give search results or an answer that is relevant and useful for the user. If you can provide valid links, provide them. If not do not add a link, instead answer the question to your best ability. Here is the query:\n"+q+"\nSearch Results: \n"
-
-
+  // fetch query from request body
+  const { q } = req.body;
+  const prompting =
+    process.env.GPT3_PROMPT +
+    q +
+    "\nSearch Results: \n";
 
   const response = await openai.createCompletion({
     model: "text-davinci-003",
@@ -31,17 +26,8 @@ const search = async (req: NextApiRequest, res: NextApiResponse) => {
     frequency_penalty: 0.5,
     presence_penalty: 0,
   });
-  console.log("api response", response.data.choices[0])
   // return openai response as JSON
-    res.status(200).json(response.data.choices[0])
-    
-}
+  res.status(200).json(response.data.choices[0]);
+};
 
-export default search
-
-
-
-
-
-
-
+export default search;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -18,13 +18,13 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by adding&nbsp;
-            <code className={styles.code}>https://searchgpt.vercel.app/search?q=%s</code> as a custom search engine in your browser. 
+            <code className={styles.code}>
+              https://searchgpt.vercel.app/search?q=%s
+            </code>{" "}
+            as a custom search engine in your browser.
           </p>
-
         </div>
-
-
       </main>
     </>
-  )
+  );
 }
