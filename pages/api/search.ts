4cfd3b34574c537c2ac7532da42b168b5bbd173e
Surya <git@sdan.io>
2023-03-28 22:24:19 -0700

    switch to 3.5 turbo

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -2,7 +2,7 @@
 // and then return the results as JSON
 
 import { NextApiRequest, NextApiResponse } from "next";
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi, ChatCompletion } = require("openai");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -17,17 +17,16 @@ const search = async (req: NextApiRequest, res: NextApiResponse) => {
     q +
     "\nSearch Results: \n";
 
-  const response = await openai.createCompletion({
+  const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    prompt: prompting,
-    temperature: 0.7,
-    max_tokens: 512,
-    top_p: 1,
-    frequency_penalty: 0.5,
-    presence_penalty: 0,
-  });
+    messages: [
+    {"role":"system","content":process.env.GPT3_PROMPT},
+    {"role":"system","content":q},
+    {"role":"system","content":"\nSearch Results: \n"},
+  ]
+});
   // return openai response as JSON
-  res.status(200).json(response.data.choices[0]);
+  res.status(200).json(response.data.choices[0].message.content);
 };
 
 export default search;
